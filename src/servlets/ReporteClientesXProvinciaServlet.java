59a0612ba21b18e20dd78f57baba3fd1e55cd86d
Ludmilaboffo <ludmilabboffo@hotmail.com>
2023-11-19 07:10:21 -0300

    Modificaciones en historial de la cuenta

--- src/servlets/ReporteClientesXProvinciaServlet.java
+++ src/servlets/ReporteClientesXProvinciaServlet.java
@@ -46,7 +46,7 @@ public class ReporteClientesXProvinciaServlet extends HttpServlet {
 				request.setAttribute("listadoClientes", clientesFiltrado);
 				request.setAttribute("cantClientes", cantidadClientes);
 				request.getRequestDispatcher("/admin/ReporteClienteXProvincia.jsp").forward(request, response);
-		}
+		} 
 	}
 
 	/**
