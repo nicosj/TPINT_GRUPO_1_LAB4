5572c00dd6758939bd0092da9638965a914746d5
seba-cali <sebastian.calicanton@alumnos.frgp.utn.edu.ar>
2023-12-06 00:19:46 -0300

    db

--- src/dao/DB.java
+++ src/dao/DB.java
@@ -15,7 +15,7 @@ public class DB {
 		{
 
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/banco2","root","");
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/banco5","root","");
 
 			this.connection.setAutoCommit(false);
 		}
